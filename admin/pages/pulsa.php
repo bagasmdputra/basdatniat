2f05cab95ecfb13a2e00808c06852ad2af1666e0
Ismail Shalih Abdul Kholiq <ismailshalih@gmail.com>
2017-05-18 00:50:49 +0700

    halaman admin, pulsa

--- admin/pages/pulsa.php
+++ admin/pages/pulsa.php
@@ -1,3 +1,7 @@
+<?php  
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -88,83 +92,44 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
+                    <h1 class="page-header">Tambah Produk (Pulsa)</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
             <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-12 text-center">
-                                    <i class="fa fa-list-ul fa-5x"></i>
-                                </div>
+                <div class="col-sm-8">
+                    <form action="pulsa.php" class="form-horizontal">
+                        <div class="form-group">
+                            <label for="kode-produk" class="control-label col-sm-4 col-lg-2">Kode Produk</label>
+                            <div class="col-sm-8 col-lg-10">
+                                <input type="text" name="kode-produk" class="form-control" required>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Buat Kategori</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-green">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-12 text-center">
-                                    <i class="fa fa-truck fa-5x"></i>
-                                </div>
+                        <div class="form-group">
+                            <label for="nama-produk" class="control-label col-sm-4 col-lg-2">Nama</label>
+                            <div class="col-sm-8 col-lg-10">
+                                <input type="text" name="nama-produk" class="form-control" required>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Tambah Jasa Kirim</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-12 text-center">
-                                    <i class="fa fa-tags fa-5x"></i>
-                                </div>
+                        <div class="form-group">
+                            <label for="harga-produk" class="control-label col-sm-4 col-lg-2">Harga</label>
+                            <div class="col-sm-8 col-lg-10">
+                                <input type="number" name="harga-produk" class="form-control" required>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Buat Promo</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-12 text-center">
-                                    <i class="fa fa-shopping-cart fa-5x"></i>
-                                </div>
+                        <div class="form-group">
+                            <label for="nominal-produk" class="control-label col-sm-4 col-lg-2">Nominal</label>
+                            <div class="col-sm-8 col-lg-10">
+                                <input type="number" name="nominal-produk" class="form-control" required>
                             </div>
                         </div>
-                        <a href="pulsa.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">Tambah Produk (Pulsa)</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
+                        <div class="form-group">
+                            <div class="text-center">
+                                <button class="btn btn-default" type="submit" value="submit">Submit</button>
                             </div>
-                        </a>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
             <!-- /.row -->
