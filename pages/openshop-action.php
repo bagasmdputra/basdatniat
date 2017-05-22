32079753141c26bf95b9a48077b6b8937cc4fa1e
Ismail Shalih Abdul Kholiq <ismailshalih@gmail.com>
2017-05-23 00:11:58 +0700

    commit aja

--- pages/openshop-action.php
+++ pages/openshop-action.php
@@ -9,7 +9,7 @@
         $deskripsi_toko = $_POST['deskripsi-toko'];
         $slogan_toko = $_POST['slogan-toko'];
         $lokasi_toko = $_POST['lokasi-toko'];
-        $jasa_kirim_toko = $_POST['jasa-kirim-toko']
+        $jasa_kirim_toko = $_POST['jasa-kirim-toko'];
 
         //cek id unique
         if(empty(pg_query_params($db, "SELECT * FROM toko WHERE nama_toko = $1",array($nama_toko)))) {
@@ -17,7 +17,7 @@
                 INSERT INTO produk_pulsa(kode_produk,nominal) VALUES ('".$kode_produk."',".$nominal_produk.");";
             pg_query_params($db, "INSERT INTO toko(nama,deskripsi,slogan,lokasi,email_penjual) VALUES($1,$2,$3,$4,$5)", array($nama_toko,$deskripsi_toko,$slogan_toko,$lokasi_toko,$_SESSION['email']));
             pg_query_params($db, "INSERT INTO toko_jasa_kirim(nama_toko,jasa_kirim) VALUES($1,$2)", array($nama_toko,$jasa_kirim_toko));
-            pg_query($db, "UPDATE pelanggan SET is_penjual = TRUE WHERE email = $_SESSION['email']");
+            pg_query_params($db, "UPDATE pelanggan SET is_penjual = TRUE WHERE email = $1", array($_SESSION['email']));
             header('Location: addproduct.php');
         }
     }
